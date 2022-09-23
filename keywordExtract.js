4a84a43454022f0863b6f96167c19c9a77828644
Shun <saitoshin2145@gmail.com>
2022-09-23 13:16:15 +0900

    Update keywordExtract.js

--- keywordExtract.js
+++ keywordExtract.js
@@ -1,5 +1,9 @@
 var keywordList = [];
 
+function userInput() {
+  var userInput = document.getElementById("userInputForm").split(",");
+  return userInput;
+}
 function tsvToArray(str, delimiter = "\t") {
   const headers = str.slice(0, str.indexOf("\r")).split(delimiter);
   for (var i = 0; i < headers.length; i++) {
@@ -109,4 +113,36 @@ function keywordCount(arr) {
   }
 
   return count;
+}
+
+function loadXMLDoc() {
+  var xmlFile = "https://www.cataloghouse.co.jp/cataloghouse_product.xml";
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open("GET", xmlFile, true);
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //getKeyWord(this);
+      getKeyWord2(this);
+    }
+  };
+  xmlhttp.open("GET", "cd_catalog.xml", true);
+  xmlhttp.send();
+}
+
+function getKeyWord2(xml) {
+  var txt = "";
+  var keywordList = [];
+  var parser = new DOMParser();
+  var xmlDoc = parser.parseFromString(xml, "text/xml");
+  var keyword = xmlDoc.getElementsByTagName("keyword");
+  for (var i = 0; i < keyword.length; i++) {
+    txt += keyword[i].childNodes[0].nodeValue + ",";
+  }
+
+  keywordList.push(txt);
+  //keyWordList = _.uniq(keywordList);
+  console.log(keywordList);
+  document.getElementById("demo").innerHTML = keywordList;
 }
